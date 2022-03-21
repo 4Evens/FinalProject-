9b3a0507bfca76e00d1e2bdec445af32842d8604
enes başarslan <enes.basarslan@gmail.com>
2022-03-21 11:40:32 +0300

    Add project files.

---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,30 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Abstract;
+
+namespace Business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        ICategoryDal _categoryDal;
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+        public List<Category> GetAll()
+        {
+            return _categoryDal.GetAll();
+        }
+
+        public Category GeyById(int categoryId)
+        {
+            return _categoryDal.Get(c => c.categoryId == categoryId);
+        }
+    }
+}
---
+++ Entities/Concrete/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Entities;
+
+namespace Entities.Concrete
+{
+    public class Category : IEntity
+    {
+        public int categoryId { get; set; }
+        public string? CategoryName { get; set; }
+    }
+}
